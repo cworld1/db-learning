2745256e7363e705834e9d8a7b0f577eb88d2be1
cworld1 <cworld0@qq.com>
2023-04-12 09:07:31 +0800

    perf: :rocket: move assets files and fix assets problem; add custom.css

--- theme.config.tsx
+++ theme.config.tsx
@@ -72,20 +72,21 @@ const config: DocsThemeConfig = {
         />
         <meta name="og:image" content="/assets/og.png" />
         <meta name="apple-mobile-web-app-title" content="Database Learning" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/icons/favicon.png" type="image/png" />
         <link
           rel="icon"
-          href="/favicon-dark.svg"
+          href="/icons/favicon-dark.svg"
           type="image/svg+xml"
           media="(prefers-color-scheme: dark)"
         />
         <link
           rel="icon"
-          href="/favicon-dark.png"
+          href="/icons/favicon-dark.png"
           type="image/png"
           media="(prefers-color-scheme: dark)"
         />
+        <link rel="stylesheet" href="/custom.css" />
       </>
     )
   },
